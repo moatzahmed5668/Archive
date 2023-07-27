90fd1188571a238a699b0974d135824e40c66b71
moatzahmed5668 <119687158+moatzahmed5668@users.noreply.github.com>
2023-07-27 23:33:21 +0300

    D - Red_Area.cpp

---
+++ D - Red_Area.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<math.h>
+#include<iomanip>
+#include<string.h>
+#include <stdio.h>
+#include <string.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+main()
+{
+    double A;
+    cin>>A;
+    cout<<fixed<<setprecision(6)<<((pow((.5*A),2)-((3.141592)*pow((.25*A),2)))/2)+(.5*.25*A*.5*A);
+
+    return 0;
+}
+
+
+
+
+
